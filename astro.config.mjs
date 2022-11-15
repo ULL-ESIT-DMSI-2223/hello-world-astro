9ec7de03787e641fe982b3dbc5716fb216848c00
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-11-15 14:55:41 +0000

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ull-esit-dmsi-2223.github.io',
+  base: '/hello-world-astro'
+});
